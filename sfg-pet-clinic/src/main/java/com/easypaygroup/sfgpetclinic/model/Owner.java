9e52917854692cda09be8e5b68cde0cc80b5753b
Kbonheur <kurt.bonheure@easypay-group.com>
2022-08-19 13:40:25 +0200

    Adding POJO's. Closes #2.

---
+++ sfg-pet-clinic/src/main/java/com/easypaygroup/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.easypaygroup.sfgpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ sfg-pet-clinic/src/main/java/com/easypaygroup/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.easypaygroup.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
